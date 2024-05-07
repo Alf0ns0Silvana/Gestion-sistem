1dc2babe3f358c86b99083f9d65008a404ffc130
Silvana A <alfonsosilvana16@gmail.com>
2024-05-07 16:37:15 -0300

    getById cliente funciona ok

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -34,10 +34,44 @@ router.post('/createClient',async (req, res, next) => {
 }); // funciona se crea ok !!!!!
 
 // Get byId clientes
-router.get('/getById/:id', (req, res, next) => { 
-});
+router.get('/:id', async (req, res, next) => { 
+    const clientId = req.params.id;
+    try {
+        const client = await clientSchema.findById(clientId);
+        if (client) {
+            res.status(200).json(client);
+        } else {
+            res.status(404).json({ message: 'Cliente no encontrado' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error obteniendo el cliente');
+    }
+}); // funciona se trae mediante su id ok !!!!!
+
 // Update cliente
-router.put('/update/:id', (req, res, next) => {
+router.put('/update/:id', async (req, res, next) => {
+    const clientId = req.params.id; // Obtiene el ID del cliente de los parámetros de la solicitud
+    const { email, name, lastname, contact, details, role } = req.body; // Obtiene los datos actualizados del cliente
+    try {
+        const updatedClient = await clientSchema.findByIdAndUpdate(clientId, {
+            email,
+            name,
+            lastname,
+            contact,
+            details,
+            role
+        }, { new: true }); // Busca/actualiza el cliente en la base de datos/devuelve cliente actualizado
+        if (updatedClient) {
+            res.status(200).json(updatedClient);// Si se actualiza correctamente:
+        } else {
+            res.status(404).json({ message: 'Cliente no encontrado' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error al actualizar el cliente' });
+    }
+
 });
 // Delete cliente
 router.delete('/delete/:id', (req, res, next) => {
